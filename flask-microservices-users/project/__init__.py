8259ce9ea4205d16e77f3776549a0a93d9141d08
SamR1 <samira.rabaaoui@coding-academy.fr>
2017-10-31 12:21:14 +0100

    Part1: fixing Dockerfile name & app config update

--- flask-microservices-users/project/__init__.py
+++ flask-microservices-users/project/__init__.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, jsonify
 
 
@@ -5,7 +6,8 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 
 # set config
-app.config.from_object('project.config.DevelopmentConfig')
+app_settings = os.getenv('APP_SETTINGS')
+app.config.from_object(app_settings)
 
 
 @app.route('/ping', methods=['GET'])
