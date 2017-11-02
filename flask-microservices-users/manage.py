92c02c86b655bff9130c274085a6ff89e277dbb8
SamR1 <samira.rabaaoui@coding-academy.fr>
2017-11-02 10:01:27 +0100

    Part1: adding some unit tests

--- flask-microservices-users/manage.py
+++ flask-microservices-users/manage.py
@@ -1,5 +1,6 @@
 from flask_script import Manager
 from project import app, db
+import unittest
 
 
 manager = Manager(app)
@@ -11,6 +12,16 @@ def recreate_db():
     db.drop_all()
     db.create_all()
     db.session.commit()
+
+
+@manager.command
+def test():
+    """Runs the tests without code coverage."""
+    tests = unittest.TestLoader().discover('project/tests', pattern='test*.py')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        return 0
+    return 1
 
 
 if __name__ == '__main__':
