7b34ffad63daa0423dd58931ff614f24c12f94ca
SamR1 <samira.rabaaoui@coding-academy.fr>
2017-11-02 10:59:23 +0100

    Part1: User routes added

--- flask-microservices-users/manage.py
+++ flask-microservices-users/manage.py
@@ -28,5 +28,13 @@ def recreate_db():
     db.session.commit()
 
 
+@manager.command
+def seed_db():
+    """Seeds the database."""
+    db.session.add(User(username='michael', email="michael@realpython.com"))
+    db.session.add(User(username='michaelherman', email="michael@mherman.org"))
+    db.session.commit()
+
+
 if __name__ == '__main__':
     manager.run()
