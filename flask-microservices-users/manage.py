a65ea00d12d4559a3f49e238ae97481c4290d9cd
SamR1 <samira.rabaaoui@coding-academy.fr>
2017-11-02 10:23:40 +0100

    Part1: use of Blueprints

--- flask-microservices-users/manage.py
+++ flask-microservices-users/manage.py
@@ -1,9 +1,23 @@
-from flask_script import Manager
-from project import app, db
 import unittest
 
+from flask_script import Manager
 
+from project import create_app, db
+from project.api.models import User
+
+
+app = create_app()
 manager = Manager(app)
+
+
+@manager.command
+def test():
+    """Runs the unit tests without test coverage."""
+    tests = unittest.TestLoader().discover('project/tests', pattern='test*.py')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        return 0
+    return 1
 
 
 @manager.command
@@ -12,16 +26,6 @@ def recreate_db():
     db.drop_all()
     db.create_all()
     db.session.commit()
-
-
-@manager.command
-def test():
-    """Runs the tests without code coverage."""
-    tests = unittest.TestLoader().discover('project/tests', pattern='test*.py')
-    result = unittest.TextTestRunner(verbosity=2).run(tests)
-    if result.wasSuccessful():
-        return 0
-    return 1
 
 
 if __name__ == '__main__':
