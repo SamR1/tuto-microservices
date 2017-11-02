ddf48d9c2bc68489b3b1c30daa8ad345dabbc775
SamR1 <samira.rabaaoui@coding-academy.fr>
2017-11-02 19:27:19 +0100

    Part2: Flask-CORS added, React updated to display users

--- flask-microservices-client/src/index.js
+++ flask-microservices-client/src/index.js
@@ -6,20 +6,31 @@ import axios from 'axios';
 class App extends Component {
   constructor() {
     super()
+    this.state = {
+      users: []
+    }
+  }
+  componentDidMount() {
+    this.getUsers();
   }
   getUsers() {
     axios.get(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`)
-    .then((res) => { console.log(res); })
+    .then((res) => { this.setState({ users: res.data.data.users }); })
     .catch((err) => { console.log(err); })
   }
   render() {
     return (
       <div className="container">
         <div className="row">
-          <div className="col-md-4">
+          <div className="col-md-6">
             <br/>
             <h1>All Users</h1>
             <hr/><br/>
+            {
+              this.state.users.map((user) => {
+                return <h4 key={user.id} className="well"><strong>{ user.username }</strong> - <em>{user.created_at}</em></h4>
+              })
+            }
           </div>
         </div>
       </div>
--- flask-microservices-users/project/__init__.py
+++ flask-microservices-users/project/__init__.py
@@ -1,5 +1,6 @@
 import os
 from flask import Flask, jsonify
+from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 
 
@@ -11,6 +12,9 @@ def create_app():
 
     # instantiate the app
     app = Flask(__name__)
+
+    # enable CORS
+    CORS(app)
 
     # set config
     app_settings = os.getenv('APP_SETTINGS')
