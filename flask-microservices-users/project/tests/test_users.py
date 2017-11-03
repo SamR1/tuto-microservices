e8419df89e8067d5c318ffabdba54834a6946c2b
SamR1 <samira.rabaaoui@coding-academy.fr>
2017-11-03 11:38:30 +0100

    Part2: users microservice - order users by date

--- flask-microservices-users/project/tests/test_users.py
+++ flask-microservices-users/project/tests/test_users.py
@@ -1,3 +1,4 @@
+import datetime
 import json
 
 from project.tests.base import BaseTestCase
@@ -5,8 +6,8 @@ from project import db
 from project.api.models import User
 
 
-def add_user(username, email):
-    user = User(username=username, email=email)
+def add_user(username, email, created_at=datetime.datetime.utcnow()):
+    user = User(username=username, email=email, created_at=created_at)
     db.session.add(user)
     db.session.commit()
     return user
@@ -122,7 +123,8 @@ class TestUserService(BaseTestCase):
 
     def test_all_users(self):
         """Ensure get all users behaves correctly."""
-        add_user('michael', 'michael@realpython.com')
+        created = datetime.datetime.utcnow() + datetime.timedelta(-30)
+        add_user('michael', 'michael@realpython.com', created)
         add_user('fletcher', 'fletcher@realpython.com')
         with self.client:
             response = self.client.get('/users')
@@ -131,10 +133,10 @@ class TestUserService(BaseTestCase):
             self.assertEqual(len(data['data']['users']), 2)
             self.assertTrue('created_at' in data['data']['users'][0])
             self.assertTrue('created_at' in data['data']['users'][1])
-            self.assertIn('michael', data['data']['users'][0]['username'])
+            self.assertIn('michael', data['data']['users'][1]['username'])
             self.assertIn(
-                'michael@realpython.com', data['data']['users'][0]['email'])
-            self.assertIn('fletcher', data['data']['users'][1]['username'])
+                'michael@realpython.com', data['data']['users'][1]['email'])
+            self.assertIn('fletcher', data['data']['users'][0]['username'])
             self.assertIn(
-                'fletcher@realpython.com', data['data']['users'][1]['email'])
+                'fletcher@realpython.com', data['data']['users'][0]['email'])
             self.assertIn('success', data['status'])
