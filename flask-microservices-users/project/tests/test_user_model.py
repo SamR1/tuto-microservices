9168aeab30926d38a61c061c99086597101d478a
SamR1 <samira.rabaaoui@coding-academy.fr>
2017-11-03 15:57:35 +0100

    Part3: unit tests refactoring

--- flask-microservices-users/project/tests/test_user_model.py
+++ flask-microservices-users/project/tests/test_user_model.py
@@ -3,17 +3,13 @@ from sqlalchemy.exc import IntegrityError
 from project import db
 from project.api.models import User
 from project.tests.base import BaseTestCase
+from project.tests.utils import add_user
 
 
 class TestUserModel(BaseTestCase):
 
     def test_add_user(self):
-        user = User(
-            username='justatest',
-            email='test@test.com',
-        )
-        db.session.add(user)
-        db.session.commit()
+        user = add_user('justatest', 'test@test.com')
         self.assertTrue(user.id)
         self.assertEqual(user.username, 'justatest')
         self.assertEqual(user.email, 'test@test.com')
@@ -21,12 +17,7 @@ class TestUserModel(BaseTestCase):
         self.assertTrue(user.created_at)
 
     def test_add_user_duplicate_username(self):
-        user = User(
-            username='justatest',
-            email='test@test.com',
-        )
-        db.session.add(user)
-        db.session.commit()
+        add_user('justatest', 'test@test.com')
         duplicate_user = User(
             username='justatest',
             email='test@test2.com',
@@ -35,14 +26,9 @@ class TestUserModel(BaseTestCase):
         self.assertRaises(IntegrityError, db.session.commit)
 
     def test_add_user_duplicate_email(self):
-        user = User(
-            username='justatest',
-            email='test@test.com',
-        )
-        db.session.add(user)
-        db.session.commit()
+        add_user('justatest', 'test@test.com')
         duplicate_user = User(
-            username='justanothertest',
+            username='justatest2',
             email='test@test.com',
         )
         db.session.add(duplicate_user)
