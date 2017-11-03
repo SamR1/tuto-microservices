f0bb58fc55d0b53af50914d06afbca8dfce79688
SamR1 <samira.rabaaoui@coding-academy.fr>
2017-11-03 11:31:08 +0100

    Part2: templates and view deleted from api

--- flask-microservices-users/project/api/views.py
+++ flask-microservices-users/project/api/views.py
@@ -97,15 +97,3 @@ def get_all_users():
         }
     }
     return jsonify(response_object), 200
-
-
-@users_blueprint.route('/', methods=['GET', 'POST'])
-def index():
-    if request.method == 'POST':
-        username = request.form['username']
-        email = request.form['email']
-        db.session.add(User(username=username, email=email))
-        db.session.commit()
-    users = User.query.order_by(User.created_at.desc()).all()
-    return render_template('index.html', users=users)
-
