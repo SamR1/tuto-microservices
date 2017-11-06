3ec587275edb133414e3bfde01d396bd6caab30e
SamR1 <samira.rabaaoui@coding-academy.fr>
2017-11-06 20:28:36 +0100

    Part3: client microservice - NavBar added

--- flask-microservices-client/src/App.jsx
+++ flask-microservices-client/src/App.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-import { Route, Switch } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom';
 import axios from 'axios';
 
 import UsersList from './components/UsersList';
 import AddUser from './components/AddUser';
 import About from './components/About';
+import NavBar from './components/NavBar';
 
 
 class App extends Component {
@@ -13,7 +14,8 @@ class App extends Component {
     this.state = {
       users: [],
       username: '',
-      email: ''
+      email: '',
+      title: 'TestDriven.io'
     }
   }
   componentDidMount() {
@@ -44,32 +46,37 @@ class App extends Component {
   }
   render() {
     return (
-      <div className="container">
-        <div className="row">
-          <div className="col-md-6">
-            <br/>
-            <Switch>
-              <Route exact path='/' render={() => (
-                <div>
-                  <h1>All Users</h1>
-                  <hr/><br/>
-                  <AddUser
-                    username={this.state.username}
-                    email={this.state.email}
-                    handleChange={this.handleChange.bind(this)}
-                    addUser={this.addUser.bind(this)}
-                  />
-                  <br/>
-                  <UsersList users={this.state.users}/>
-                </div>
-              )} />
-              <Route exact path='/about' component={About}/>
-            </Switch>
+      <div>
+        <NavBar
+          title={this.state.title}
+        />
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6">
+              <br/>
+              <Switch>
+                <Route exact path='/' render={() => (
+                  <div>
+                    <h1>All Users</h1>
+                    <hr/><br/>
+                    <AddUser
+                      username={this.state.username}
+                      email={this.state.email}
+                      handleChange={this.handleChange.bind(this)}
+                      addUser={this.addUser.bind(this)}
+                    />
+                    <br/>
+                    <UsersList users={this.state.users}/>
+                  </div>
+                )} />
+                <Route exact path='/about' component={About}/>
+              </Switch>
+            </div>
           </div>
         </div>
       </div>
-    );
-  }
+    )
+  };
 }
 
 export default App
