a76d0fbd3712dafe947f51138d8f63ba62acc6ce
SamR1 <samira.rabaaoui@coding-academy.fr>
2017-11-06 15:20:58 +0100

    Part3: users microservice - Adding first auth route: register

---
+++ flask-microservices-users/project/api/auth.py
@@ -0,0 +1,58 @@
+from flask import Blueprint, jsonify, request
+from sqlalchemy import exc, or_
+
+from project.api.models import User
+from project import db, bcrypt
+
+
+auth_blueprint = Blueprint('auth', __name__)
+
+
+@auth_blueprint.route('/auth/register', methods=['POST'])
+def register_user():
+    # get post data
+    post_data = request.get_json()
+    if not post_data:
+        response_object = {
+            'status': 'error',
+            'message': 'Invalid payload.'
+        }
+        return jsonify(response_object), 400
+    username = post_data.get('username')
+    email = post_data.get('email')
+    password = post_data.get('password')
+    try:
+        # check for existing user
+        user = User.query.filter(
+            or_(User.username == username, User.email == email)).first()
+        if not user:
+            # add new user to db
+            new_user = User(
+                username=username,
+                email=email,
+                password=password
+            )
+            db.session.add(new_user)
+            db.session.commit()
+            # generate auth token
+            auth_token = new_user.encode_auth_token(new_user.id)
+            response_object = {
+                'status': 'success',
+                'message': 'Successfully registered.',
+                'auth_token': auth_token.decode()
+            }
+            return jsonify(response_object), 201
+        else:
+            response_object = {
+                'status': 'error',
+                'message': 'Sorry. That user already exists.'
+            }
+            return jsonify(response_object), 400
+    # handler errors
+    except (exc.IntegrityError, ValueError) as e:
+        db.session.rollback()
+        response_object = {
+            'status': 'error',
+            'message': 'Invalid payload.'
+        }
+        return jsonify(response_object), 400
